39c8535c2fc198506904ddcbf7aee35817dcaa4f
Vion Nguyen <hiepnm@vmodev.com>
2023-05-17 17:57:51 +0700

    update SEO

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,16 +8,16 @@ export default function Home() {
     <>
       <Head>
         <title>Tuc Mach Restaurant</title>
-        <meta
-          name="title"
-          content="Nhà Hàng Túc Mạch - Đậm Chất, Đậm Vị"
-        />
+        <meta name="og:title" content="Nhà Hàng Túc Mạch - Đậm Chất, Đậm Vị" />
         <meta name="og:description" content="Hân Hạnh Phục Vụ Quý Khách" />
         <meta
-          name="author"
+          name="og:author"
           content="Vion Nguyen - facebook.com/minhhiep.vip99"
         />
-        <meta name="image" content="http://7oroof.com/demos/steakin/assets/images/sliders/slide-bg/2.jpg" />
+        <meta
+          name="og:image"
+          content="http://7oroof.com/demos/steakin/assets/images/sliders/slide-bg/2.jpg"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <HomePage />

f3d7d821027f3cf2151847d093955dcb1ceddcd3
Vion Nguyen <hiepnm@vmodev.com>
2023-06-30 15:30:19 +0700

    update section 3 home page

--- src/components/home/section3/index.tsx
+++ src/components/home/section3/index.tsx
@@ -2,8 +2,6 @@ import React, { FC } from "react";
 import classes from "./section3.module.scss";
 import { Row } from "reactstrap";
 
-const slides = ['Slide 1', 'Slide 2', 'Slide 3'];
-
 const Section3: FC = () => {
   return (
     <div className={classes.Container}>
